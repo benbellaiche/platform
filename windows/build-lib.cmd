eed559966720b19318bb06a140323ed1be06bd9a
Benjamin BELLAICHE <benjamin.bellaiche@hotmail.fr>
2020-01-05 16:10:05 +0100

    Fix space

--- windows/build-lib.cmd
+++ windows/build-lib.cmd
@@ -5,20 +5,26 @@ rem Build the p8 platform library for Windows
 SETLOCAL
 
 SET MYDIR=%~dp0
-SET BUILDARCH=%1
-SET BUILDTYPE=%2
-SET VSVERSION=%3
-SET INSTALLPATH=%4
-IF [%4] == [] GOTO missingparams
+SET PROJECTTYPE=%1
+SET BUILDARCH=%2
+SET BUILDTYPE=%3
+SET VSVERSION=%4
+SET INSTALLPATH=%~5
 
 SET BUILDTARGET=%INSTALLPATH%\cmake\%BUILDARCH%
 SET TARGET=%INSTALLPATH%\%BUILDARCH%
 
-call %MYDIR%..\support\windows\cmake\generate.cmd %BUILDARCH% nmake %MYDIR%..\ %BUILDTARGET% %TARGET% %BUILDTYPE% %VSVERSION% static
-call %MYDIR%..\support\windows\cmake\build.cmd %BUILDARCH% %BUILDTARGET% %VSVERSION%
+IF [%5] == [] GOTO missingparams
+
+call "%MYDIR%..\support\windows\cmake\generate.cmd" %BUILDARCH% %PROJECTTYPE% "%MYDIR%.." "%BUILDTARGET%" "%TARGET%" %BUILDTYPE% %VSVERSION% static
+
+IF %PROJECTTYPE% == nmake (
+    call "%MYDIR%..\support\windows\cmake\build.cmd" %BUILDARCH% "%BUILDTARGET%" %VSVERSION%
+)
+
 goto exit
 
 :missingparams
-echo "build-lib.cmd requires 4 parameters"
+echo "build-lib.cmd requires 5 parameters"
 
 :exit
--- windows/build.cmd
+++ windows/build.cmd
@@ -5,14 +5,18 @@ rem Build the p8 platform library for Windows
 SETLOCAL
 
 SET MYDIR=%~dp0
+SET PROJECTTYPE=nmake
 SET BUILDTYPE=Release
-SET VSVERSION=12
+SET VSVERSION=16
 SET INSTALLPATH=%MYDIR%..\build
 
-rmdir %MYDIR%..\build /s /q
+if exist "%MYDIR%..\build" rmdir "%MYDIR%..\build" /s /q
 
-for %%T in (amd64 x86) do (
-  call %MYDIR%\build-lib.cmd %%T %BUILDTYPE% %VSVERSION% %INSTALLPATH%
+for %%T in (x64) do (
+  call "%MYDIR%\build-lib.cmd" %PROJECTTYPE% %%T %BUILDTYPE% %VSVERSION% "%INSTALLPATH%"
 )
 
-rmdir %MYDIR%..\build\cmake /s /q
+IF %PROJECTTYPE% == nmake (
+  if exist "%MYDIR%..\build\cmake" rmdir "%MYDIR%..\build\cmake" /s /q
+)
+
